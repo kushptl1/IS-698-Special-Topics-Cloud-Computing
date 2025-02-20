fc377af8a3c366891c2912b54c7f2e914cfae547
kushptl1 <kushp1@umbc.edu>
2025-02-20 01:36:35 -0500

    Amazon S3 Activities Using Python Boto3

---
+++ delete_bucket.py
@@ -0,0 +1,9 @@
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'my-boto3-s3-bucket-kushpatel'
+
+s3.delete_bucket(Bucket=bucket_name)
+print(f'Bucket {bucket_name} deleted successfully!')
+
+
